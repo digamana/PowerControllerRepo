cf1f455458a2a9966b6bc0d402a80a490167bbf9
q12389 <q12389@yahoo.com.tw>
2022-02-16 22:14:06 +0800

    建立Winform 初始化 並添加美化UIO的Krypton物件

--- PowerController/PowerController/Program.cs
+++ PowerController/PowerController/Program.cs
@@ -1,17 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace PowerController
 {
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new PowerController());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
